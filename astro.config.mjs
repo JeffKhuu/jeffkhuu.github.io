e7df3660ef6b569dfefe3ed51447491c3f7857fa
Jeff Khuu <heyjeffkhuu@gmail.com>
2025-08-10 22:51:27 -0600

    configure for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jeffkhuu.github.io',  
+
+
   vite: {
       plugins: [tailwindcss()],
   },
